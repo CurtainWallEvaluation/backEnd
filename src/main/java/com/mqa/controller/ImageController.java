9a9fc3c8d6e8755a4a4fccd80d857111e60cee64
topologicalstructure <1876311185@qq.com>
2024-01-06 17:33:21 +0800

    更正

--- src/main/java/com/mqa/controller/ImageController.java
+++ src/main/java/com/mqa/controller/ImageController.java
@@ -5,10 +5,8 @@ import com.mqa.entity.Result;
 import com.mqa.service.OriginalImageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/image")
 @Tag(name = "原图相关接口")
@@ -17,28 +15,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImageController {
     private final OriginalImageService originalImageService;
 
+    @Autowired
     public ImageController(OriginalImageService originalImageService) {
         this.originalImageService = originalImageService;
     }
 
     @GetMapping("/getImgCurtainInfo/{oriImgID}")
-    public Result<String> getImgCurtainInfo(@PathVariable("oriImgID") String oriImgID) {
+    public Result<String> getImgCurtainInfo(@PathVariable("oriImgID") int oriImgID) {
         log.info("get image curtain info, oriImgID:{}", oriImgID);
 
         return Result.success("get image curtain info success");
     }
 
     @GetMapping("/getOriImgStatic/{oriImgID}")
-    public Result<String> getOriImgStatic(@PathVariable("oriImgID") String oriImgID) {
+    public Result<String> getOriImgStatic(@PathVariable("oriImgID") Integer oriImgID) {
         log.info("get image static info, oriImgID:{}", oriImgID);
 
         return Result.success("get image curtain info success");
     }
 
     @GetMapping("/getOriImgGlassStatic/{oriImgID}")
-    public Result<String> getOriImgGlassStatic(@PathVariable("oriImgID") String oriImgID) {
+    public Result<GlassStaticDto> getOriImgGlassStatic(@PathVariable("oriImgID") Integer oriImgID) {
         log.info("get image glass static info, oriImgID:{}", oriImgID);
-        GlassStaticDto resultData=originalImageService.findGlassStatic(Integer.parseInt(oriImgID));
-        return Result.success(resultData.toString());
+        GlassStaticDto resultData=originalImageService.findGlassStatic(oriImgID);
+        return Result.success(resultData);
     }
 }
--- src/main/java/com/mqa/service/OriginalImageService.java
+++ src/main/java/com/mqa/service/OriginalImageService.java
@@ -5,6 +5,7 @@ import com.mqa.dto.GlassStaticDto;
 import com.mqa.mapper.OriginalImageMapper;
 import com.mqa.mapper.StoneImageMapper;
 import com.mqa.mapper.GlassImageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class OriginalImageService {
     private final StoneImageMapper stoneImageMapper;
     private final GlassImageMapper glassImageMapper;
 
+    @Autowired
     public OriginalImageService(OriginalImageMapper originalImageMapper, StoneImageMapper stoneImageMapper, GlassImageMapper glassImageMapper) {
         this.originalImageMapper = originalImageMapper;
         this.stoneImageMapper = stoneImageMapper;
@@ -23,11 +25,11 @@ public class OriginalImageService {
         int[] returnList=new int[2];
         QueryWrapper wrapper1 = new QueryWrapper();
         wrapper1.eq("original_image_id",oriID);
-        wrapper1.eq("isImplosion",1);
+        wrapper1.eq("is_implosion",1);
         returnList[0]= Math.toIntExact(glassImageMapper.selectCount(wrapper1));
         QueryWrapper wrapper2 = new QueryWrapper();
         wrapper2.eq("original_image_id",oriID);
-        wrapper2.eq("isImplosion",0);
+        wrapper2.eq("is_implosion",0);
         returnList[1]= Math.toIntExact(glassImageMapper.selectCount(wrapper2));
         GlassStaticDto returnData=new GlassStaticDto();
         returnData.setData(returnList);

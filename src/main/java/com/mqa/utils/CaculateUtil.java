38073e3c6aabccb694f64957f14c106a8915a9d7
gyy <g18355735449@163.com>
2024-01-10 18:38:11 +0800

    添加计算相关方法

---
+++ src/main/java/com/mqa/utils/CaculateUtil.java
@@ -0,0 +1,32 @@
+package com.mqa.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CaculateUtil {
+    private static double[] weight;
+
+    private static double[] stainProportion;
+    private static double[] stainColorDiffer;
+    private static int[] crackNum;
+    private static double[] lengthCaculated;
+    private static double[] widthCaculated;
+    private static double[] crackAreaPercent;
+
+
+
+    /**
+     * 计算权重
+     */
+    public static void caculateWeight() {
+
+    }
+
+    /**
+     * 计算韧性得分
+     */
+    public static Double caculatePoint() {
+    }
+
+
+}
